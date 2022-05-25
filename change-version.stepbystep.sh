a521a3d19a05048dd71244fb86160e1c563a07d4
Cyrille Hamel <cyrille.hamel@kicknride.fr>
2022-05-25 08:09:31 +0200

    add change step file

---
+++ change-version.stepbystep.sh
@@ -0,0 +1,52 @@
+echo "amd64+firmwares/"
+sed -i 's/LibraZiK-3/LibraZiK-4/g' amd64+firmwares/auto/config
+sed -i 's/LibraZiK3/LibraZiK4/g' amd64+firmwares/auto/config
+sed -i 's/#3 pas à pas/#4 barbe à papas/g' amd64+firmwares/auto/config
+sed -i 's/buster/bullseye/g' amd64+firmwares/auto/config
+sed -i 's/buster/bullseye/g' amd64+firmwares/config/archives/backport.pref.binary
+sed -i 's/buster/bullseye/g' amd64+firmwares/config/archives/backport.pref.chroot
+sed -i 's/LZK3/LZK4/g' amd64+firmwares/config/archives/librazik.list.chroot
+sed -i 's/LibraZiK3/LibraZiK4/g' amd64+firmwares/config/archives/librazik.list.chroot
+sed -i 's/pasapas/barbeapapas/g' amd64+firmwares/config/archives/librazik.list.chroot
+sed -i 's/LZK-PAP-users/LZK-BAP-users/g' amd64+firmwares/config/archives/librazik.list.chroot
+sed -i 's/#3 "pas a pas/#4 "barbe à papas/g' amd64+firmwares/config/archives/librazik.list.chroot
+sed -i 's/4.19.0-12/5.10.0-14/g' amd64+firmwares/config/package-lists/librazik-base.list.chroot
+sed -i 's/gtklick/gtick/g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/scribus-ng/scribus/g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/remid-lv2//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/talentedhack//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/vcvrack//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/wolf-shaper//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/wolf-spectrum//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-gmsynth.lv2//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-mclk.lv2//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-avldrums.lv2//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-plugins//g' amd64+firmwares/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/LibraZiK studio #4 barbe à papas/LibraZiK std #4 barbe à papas/g' amd64+firmwares/auto/config
+
+
+echo "amd64+firmwares+en/"
+sed -i 's/LibraZiK-3/LibraZiK-4/g' amd64+firmwares+en/auto/config
+sed -i 's/LibraZiK3/LibraZiK4/g' amd64+firmwares+en/auto/config
+sed -i 's/#3 pas à pas/#4 barbe à papas/g' amd64+firmwares+en/auto/config
+sed -i 's/buster/bullseye/g' amd64+firmwares+en/auto/config
+sed -i 's/buster/bullseye/g' amd64+firmwares+en/config/archives/backport.pref.binary
+sed -i 's/buster/bullseye/g' amd64+firmwares+en/config/archives/backport.pref.chroot
+sed -i 's/LZK3/LZK4/g' amd64+firmwares+en/config/archives/librazik.list.chroot
+sed -i 's/LibraZiK3/LibraZiK4/g' amd64+firmwares+en/config/archives/librazik.list.chroot
+sed -i 's/pasapas/barbeapapas/g' amd64+firmwares+en/config/archives/librazik.list.chroot
+sed -i 's/LZK-PAP-users/LZK-BAP-users/g' amd64+firmwares+en/config/archives/librazik.list.chroot
+sed -i 's/#3 "pas a pas/#4 "barbe à papas/g' amd64+firmwares+en/config/archives/librazik.list.chroot
+sed -i 's/4.19.0-12/5.10.0-14/g' amd64+firmwares+en/config/package-lists/librazik-base.list.chroot
+sed -i 's/gtklick/gtick/g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/scribus-ng/scribus/g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/remid-lv2//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/talentedhack//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/vcvrack//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/wolf-shaper//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/wolf-spectrum//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-gmsynth.lv2//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-mclk.lv2//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-avldrums.lv2//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/x42-plugins//g' amd64+firmwares+en/config/package-lists/librazik-logiciels.list.chroot
+sed -i 's/LibraZiK studio #4 barbe à papas/LibraZiK std #4 barbe à papas/g' amd64+firmwares+en/auto/config
